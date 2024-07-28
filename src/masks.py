5158ed5957931cc01f02d15be13af737d954a155
Nikita Khramov <nikuser12321@gmail.com>
2024-07-28 06:24:28 +0300

    Упростил функции

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,10 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция заменяет нужные числа на '*' и добавляет пробелы"""
-    symbols: list[str] = []
-    for symbol in card_number:
-        symbols += symbol
-    count: int = 0
-    for number in range(len(symbols)):
-        if 6 <= count <= 11:
-            symbols[number] = "*"
-        count += 1
-    for index in range(3):
-        symbols.insert((-4) * (index + 1) - index, " ")
-    mask_card_number: str = "".join(symbols)
+    mask_card_number: str = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     return mask_card_number
 
 
 def get_mask_account(account: str) -> str:
     """Функция выводит последние 4 символа и добавляет '**' в начало"""
-    end_part: str = account[-4:]
-    result: str = "**" + end_part
-    return result
+    mask_account: str = f"**{account[-4:]}"
+    return mask_account
