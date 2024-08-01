897b495e0d922a74ceba0a705e2baf035135dee3
Nikita Khramov <nikuser12321@gmail.com>
2024-08-01 21:20:15 +0300

    Added sort_by_date funktion

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,11 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
         if dct["state"] == state:
             new_list.append(dct)
     return new_list
+
+
+def sort_by_date(dct_list: list, state: bool = True) -> list:
+    """
+    Функция сортирует список словарей по ключу 'date'
+    """
+    new_list: list = sorted(dct_list, key=lambda dct: dct["date"], reverse=state)
+    return new_list
