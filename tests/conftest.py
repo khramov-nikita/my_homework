0ea0f38448780e226390409f95b98e7add329a26
Nikita Khramov <nikuser12321@gmail.com>
2024-08-13 23:19:18 +0300

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,9 +48,7 @@ def transactions() -> list[dict]:
             "description": "Перевод организации",
             "from": "Visa Platinum 1246377376343588",
             "to": "Счет 14211924144426031657",
-        },
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        }
     ]
 
 
