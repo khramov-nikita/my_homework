3a15f9ce0ab949b7cfc4bca7d54e5bc62c1eb3ff
Nikita Khramov <nikuser12321@gmail.com>
2024-08-21 23:37:19 +0300

    Reformated test_external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -14,32 +14,35 @@ def test_transaction_sum(mock_get: Any) -> None:
         "result": 751022.33037,
     }
     assert (
-            transaction_sum(
-                {
-                    "id": 41428829,
-                    "state": "EXECUTED",
-                    "date": "2019-07-03T18:35:29.512364",
-                    "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
-                    "description": "Перевод организации",
-                    "from": "MasterCard 7158300734726758",
-                    "to": "Счет 35383033474447895560",
-                }
-            )
-            == 751022.33037
+        transaction_sum(
+            {
+                "id": 41428829,
+                "state": "EXECUTED",
+                "date": "2019-07-03T18:35:29.512364",
+                "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+                "description": "Перевод организации",
+                "from": "MasterCard 7158300734726758",
+                "to": "Счет 35383033474447895560",
+            }
+        )
+        == 751022.33037
     )
 
 
 @patch("requests.get")
 def test_transactions_sum_rub(mock_get: Any) -> None:
-    assert transaction_sum(
-        {
-            "id": 441945886,
-            "state": "EXECUTED",
-            "date": "2019-08-26T10:50:58.294041",
-            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
-            "description": "Перевод организации",
-            "from": "Maestro 1596837868705199",
-            "to": "Счет 64686473678894779589",
-        }
-    ) == 31957.58
+    assert (
+        transaction_sum(
+            {
+                "id": 441945886,
+                "state": "EXECUTED",
+                "date": "2019-08-26T10:50:58.294041",
+                "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+                "description": "Перевод организации",
+                "from": "Maestro 1596837868705199",
+                "to": "Счет 64686473678894779589",
+            }
+        )
+        == 31957.58
+    )
     mock_get.assert_not_called()
