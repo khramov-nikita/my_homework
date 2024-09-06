22fdc42b35d9a92358ab1b945102acc610ef62c5
Nikita Khramov <nikuser12321@gmail.com>
2024-09-06 03:31:29 +0300

    Added reading

---
+++ src/reading.py
@@ -0,0 +1,19 @@
+import csv
+
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+
+def read_csv(path: str) -> list:
+    result = []
+    with open(path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            result.append(row)
+        return result
+
+
+def read_excel(path: str) -> list:
+    excel_data: DataFrame = pd.read_excel(path)
+    result: list = excel_data.to_dict(orient="records")
+    return result
